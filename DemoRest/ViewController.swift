c20f921adee6d426e573d59798f554c54dbd9083
mobapp11 <mobapp11@dt-c205-22.local>
2019-01-29 11:42:13 +0100

    finished tableView to show all posts

--- DemoRest/ViewController.swift
+++ DemoRest/ViewController.swift
@@ -10,15 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var items = [UserPost].init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print (PostParser.getAllPosts())
-        
-        
-        
+        items = PostParser.getAllPosts()
     }
-
-
 }
 
+extension ViewController:UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        
+        let userPostForCell = items[indexPath.row]
+        
+        cell.textLabel!.text = userPostForCell.title
+        
+        
+        return cell
+    }
+
+}
